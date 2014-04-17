cc01136bc5e83610e643f5daaebf112cae4382a4
Gargaj <gargaj@conspiracy,hu>
2014-04-17 16:00:08 +0200

    add noise texture

--- liveCoding/liveCoding_inputs.cpp
+++ liveCoding/liveCoding_inputs.cpp
@@ -10,6 +10,7 @@ void liveCoding::setupShaderInputs()
 {
 	textures_.push_back( Tex( "iSplashScreen", "textures/splashScreen.png" ) );
 	textures_.push_back( Tex( "iChecker", "textures/checker.png" ) );
+  textures_.push_back( Tex( "iNoise", "textures/noise.png" ) );
 	textures_.push_back( Tex( "iTex1", "textures/tex1.png" ) );
 	textures_.push_back( Tex( "iTex2", "textures/tex2.png" ) );
 	textures_.push_back( Tex( "iTex3", "textures/tex3.png" ) );
